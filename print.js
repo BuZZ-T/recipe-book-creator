6973afae5632cd6a2f10f60203da1633c29945a1
Bastian Gebhardt <Bastian.Gebhardt@jambit.com>
2021-01-06 15:39:39 +0100

    Use Query Parameter instead file editing for setting pages in print.html

--- print.js
+++ print.js
@@ -1,29 +1,13 @@
 const puppeteer = require('puppeteer')
-const { readFile, writeFile } = require('fs')
 const program = require('commander')
-const { promisify } = require('util')
 const { join } = require('path')
 const packageJson = require('./package.json')
-
-const readFilePromise = promisify(readFile)
-const writeFilePromise = promisify(writeFile)
 
 const defaultPapersPerSections = '6'
 const defaultSections = '8'
 
 const PRINT_HTML_FILENAME = 'print.html'
 const printHtmlPath = join(__dirname, PRINT_HTML_FILENAME)
-
-async function replacePageNumber({ first, second, secondLast, last, hasLastPage }) {
-    const printHtmlFile = await readFilePromise(printHtmlPath, 'utf-8')
-    const modifiedPrintHtmlFile = printHtmlFile.replace(/const first = '\d+'/, `const first = '${first}'`)
-        .replace(/const second = '\d+'/, `const second = '${second}'`)
-        .replace(/const secondLast = '\d+'/, `const secondLast = '${secondLast}'`)
-        .replace(/const last = '\d+'/, `const last = '${last}'`)
-        .replace(/const lastPage = .*/, `const lastPage = ${hasLastPage}`)
-
-    await writeFilePromise(printHtmlPath, modifiedPrintHtmlFile)
-}
 
 function parseArgs() {
     program
@@ -56,27 +40,37 @@ async function printAll() {
             const secondLast = lastPageOfSection - page + pageStart
             const last = lastPageOfSection - page + pageStart + 1
 
-            await replacePageNumber({
+            await printPage({
+                pageNumber: outputFileCounter,
                 first,
                 second,
                 secondLast,
                 last,
                 hasLastPage: section === config.sections - 1 && page === 1
             })
-            await printPage(outputFileCounter)
             outputFileCounter++
         }
     }
 }
 
-async function printPage(pageNumber) {
+async function printPage({
+    pageNumber,
+    first,
+    second,
+    secondLast,
+    last,
+    hasLastPage,
+}) {
     console.log(`print page ${pageNumber}`)
     const browser = await puppeteer.launch({
         headless: true,
         args: ['--disable-web-security', '--user-data-dir=chrome-user']
     });
+
+    const query = `first=${first}&second=${second}&secondLast=${secondLast}&last=${last}&hasLastPage=${hasLastPage}`
+    
     const page = await browser.newPage();
-    await page.goto(`file://${printHtmlPath}`, { waitUntil: 'networkidle0' });
+    await page.goto(`file://${printHtmlPath}?${query}`, { waitUntil: 'networkidle0' });
 
     await page.pdf({
         format: 'A4',
